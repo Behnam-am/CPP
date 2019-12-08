0c5c14b9cb47a3d0d931de728f8a2ceab3edcde1
Behnam-am <56001519+Behnam-am@users.noreply.github.com>
2019-12-08 21:07:35 +0330

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,27 @@
+﻿// There are 10 types of people in this world. Those who understand binary and those who don't.
+// برنامه تشخیص متقارن بودن یک عدد
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	
+	int n, r, nCopy, result = 0;
+	cin >> n;
+	nCopy = n;
+
+	while (n > 0)
+	{
+		r = n % 10;
+		n = n / 10;
+		result = result * 10 + r;
+	}
+
+	if (nCopy == result) cout << "YES";
+	else cout << "NO";
+
+
+	return 0;
+}
