8fc414e2441565c71f098517ea42cab2136ac556
Behnam-am <56001519+Behnam-am@users.noreply.github.com>
2019-12-08 19:32:15 +0330

    Update rondBeKoochektar.cpp

--- rondBeKoochektar.cpp
+++ rondBeKoochektar.cpp
@@ -1,7 +1,6 @@
 // There are 10 types of people in this world. Those who understand binary and those who don't.
 // برنامه رند کردن اعداد به اولین عدد صحیح کوچکتر
 
-
 #include <iostream>
 
 using namespace std;
