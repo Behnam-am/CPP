f5bee81caa606138035b9a6537548a2d15f39935
Behnam-am <56001519+Behnam-am@users.noreply.github.com>
2019-12-13 19:14:24 +0330

    Create gcd.cpp

---
+++ gcd.cpp
@@ -0,0 +1,27 @@
+﻿// There are 10 types of people in this world. Those who understand binary and those who don't.
+// برنامه ای که ب م م دو عدد را محاسبه میکند
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	
+	int a, b, c, result;
+	cin >> a >> b;
+	if (a < b)
+		swap(a, b);
+	do {
+		result = b;
+		c = a % b;
+		a = b;
+		b = c;
+	} while (c > 0);
+
+	cout << result;
+	return 0;
+}
+
+
+
