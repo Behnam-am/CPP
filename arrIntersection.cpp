dc6c9ded6e7f91c2be63ea4462a6d4f3d85e5e60
Behnam-am <56001519+Behnam-am@users.noreply.github.com>
2019-12-23 23:33:23 +0330

    Create arrIntersection.cpp

---
+++ arrIntersection.cpp
@@ -0,0 +1,61 @@
+﻿// There are 10 types of people in this world. Those who understand binary and those who don't.
+// برنامه ای که اشتراک دو آرایه را محاسبه کند
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+bool isInArr(float[], float);
+
+const int arrSize = 10;
+
+int main() {
+
+	float arr1[arrSize], arr2[arrSize], intersectionArray[arrSize], x;
+	int interLen;
+	// get arr1 elements
+	cout << "Enter " << arrSize << " numbers for array 1:\n";
+	for (int i = 0; i < arrSize; i++)
+	{
+		cin >> arr1[i];
+	}
+	// get arr2 elements
+	cout << "Enter " << arrSize << " numbers for array 2:\n";
+	for (int i = 0; i < arrSize; i++)
+	{
+		cin >> arr2[i];
+	}
+
+	// calculate intersection Array
+	interLen = 0;
+	for (int i = 0; i < arrSize; i++)
+	{
+		if (isInArr(arr2, arr1[i]) && !(isInArr(intersectionArray, arr1[i])))
+		{
+			intersectionArray[interLen] = arr1[i];
+			interLen++;
+		}
+	}
+
+	// show intersection array
+	cout << "Intersection array = [";
+	for (int i = 0; i < interLen; i++)
+	{
+		cout << intersectionArray[i] << ", ";
+	}
+	cout << "\b\b]" << endl;
+
+	return 0;
+}
+
+bool isInArr(float arr[], float x)
+{
+	for (int i = 0; i < arrSize; i++)
+	{
+		if (arr[i] == x)
+		{
+			return true;
+		}
+	}
+	return false;
+}
