69c3093bf6e40962e68f07e6f3e1ed2a784737b2
Behnam-am <56001519+Behnam-am@users.noreply.github.com>
2019-12-23 23:33:18 +0330

    Create arrDeleteDups.cpp

---
+++ arrDeleteDups.cpp
@@ -0,0 +1,45 @@
+﻿// There are 10 types of people in this world. Those who understand binary and those who don't.
+// برنامه ای که عناصر تکراری آرایه را حذف کند
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+const int arrSize = 10;
+
+int main() {
+
+	float grades[arrSize], x;
+	int j = 0;
+
+	// get grades elements
+	cout << "Enter " << arrSize << " grades:\n";
+	for (int i = 0; i < arrSize; i++)
+	{
+		cin >> grades[i];
+	}
+
+	for (int i = 0; i < arrSize; i++)
+	{
+		for (int j = i + 1; j < arrSize; j++)
+		{
+			if (grades[i] == grades[j])
+			{
+				grades[j] = -1;
+			}
+		}
+	}
+	
+	// show grades array
+	cout << "Your array with no dupicate = [";
+	for (int i = 0; i < arrSize; i++)
+	{
+		if (grades[i] >= 0) {
+			cout << grades[i] << ", ";
+		}
+	}
+	cout << "\b\b]" << endl;
+
+	return 0;
+}
+
