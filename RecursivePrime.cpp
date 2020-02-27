e1f77f747a81c0faf1082c44b4d4e971e128d48a
Behnam-am <56001519+Behnam-am@users.noreply.github.com>
2020-02-27 13:52:56 +0330

    Create RecursivePrime.cpp

---
+++ RecursivePrime.cpp
@@ -0,0 +1,36 @@
+﻿// There are 10 types of people in this world. Those who understand binary and those who don't.
+/*
+	تابع بازگشتی که اول بودن عدد را چک کند
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPrime(int, int);
+
+int main()
+{
+	int x;
+	string ans;
+	cin >> x;
+	ans = (isPrime(x, x / 2) == true) ? "Prime" : "Not Prime";
+	cout << ans;
+	return 0;
+}
+
+bool isPrime(int x, int d)
+{
+	if (d == 1)
+	{
+		return true;
+	}
+	else if (x % d == 0)
+	{
+		return false;
+	}
+	else
+	{
+		return isPrime(x, d - 1);
+	}
+}
