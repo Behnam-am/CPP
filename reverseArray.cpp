79e9cef6eb737227b0209a801243f7be957a3d7f
Behnam-am <56001519+Behnam-am@users.noreply.github.com>
2020-02-27 03:53:23 +0330

    Create reverseArray.cpp

---
+++ reverseArray.cpp
@@ -0,0 +1,28 @@
+﻿// There are 10 types of people in this world. Those who understand binary and those who don't.
+/*
+	برنامه ای که آرایه ای را دریافت کرده و عناصر آن را نسبت به وسط قرینه کند
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int arrSize = 9;
+	int arr[arrSize];
+	for (int i = 0; i < arrSize; i++)
+	{
+		cin >> arr[i];
+	}
+	for (int i = 0; i < arrSize/2; i++)
+	{
+		swap(arr[i], arr[arrSize - 1 - i]);
+	}
+	for (int i = 0; i < arrSize; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+}
+
