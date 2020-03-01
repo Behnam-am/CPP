558b0a276f4d773a47ca6e5b70d91c8ebb9610cd
Behnam-am <56001519+Behnam-am@users.noreply.github.com>
2020-03-02 01:16:34 +0330

    Create mixTwoStrings.cpp

---
+++ mixTwoStrings.cpp
@@ -0,0 +1,23 @@
+﻿// There are 10 types of people in this world. Those who understand binary and those who don't.
+/*
+	برنامه ای که حروف یک رشته را به صورت یک در میان در رشته دیگر قرار دهد
+	"abcdef" & "xy" ==> axbycxdyexf
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str = "abcdef", substr = "xy", finalStr;
+	for (int i = 0; i < str.length(); i++)
+	{
+		finalStr += str[i];
+		finalStr += substr[i % substr.length()];
+	}
+	finalStr.erase(finalStr.length() - 1);
+	cout << finalStr;
+	return 0;
+}
+
