669c350724f5b416bcfab6a39611e7fc035b1d20
Behnam-am <56001519+Behnam-am@users.noreply.github.com>
2020-05-09 02:59:37 +0430

    Create reverseTextButNotWords.cpp

---
+++ reverseTextButNotWords.cpp
@@ -0,0 +1,30 @@
+﻿// There are 10 types of people in this world. Those who understand binary and those who don't.
+/*
+	reverse text but not words
+*/
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	string str1, word, str2 = "";
+
+	getline(cin, str1);
+	stringstream phrase(str1);
+
+	while (getline(phrase, word, ' '))
+	{
+		str2 = word + " " + str2;
+	}
+
+	// remove last space
+	str2.erase(str1.length());
+
+	cout << str2;
+
+	return 0;
+}
