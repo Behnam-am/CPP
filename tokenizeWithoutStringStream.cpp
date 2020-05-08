29c3b1a99306e0bfee2e7199c8204c709f884c0f
Behnam-am <56001519+Behnam-am@users.noreply.github.com>
2020-05-09 03:31:33 +0430

    Create tokenizeWithoutStringStream.cpp

---
+++ tokenizeWithoutStringStream.cpp
@@ -0,0 +1,25 @@
+﻿// There are 10 types of people in this world. Those who understand binary and those who don't.
+/*
+	tokenize a string Without using stringstream
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str1 = "tokenize a string Without using stringstream", str2, word;
+	int loc;
+	while (str1.find(" ") != -1)
+	{
+		loc = str1.find(" ");
+		word = str1.substr(0, loc);
+		cout << word << endl;
+		str1.erase(0, loc + 1);
+	}
+
+	return 0;
+}
+
